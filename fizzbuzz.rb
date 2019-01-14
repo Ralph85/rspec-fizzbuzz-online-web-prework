f20f6ca5bea9976536dc50b2fdd314b1530a40cc
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-14 04:49:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,11 @@ require 'rspec'
 class FizzBuzz
   def fizz_buzz(number)
     result = ''
-    result = 'Fizz' if number % 3 == 0
-    result = 'Buzz' if number % 5 == 0
+    result += 'Fizz' if number % 3 == 0
+    result += 'Buzz' if number % 5 == 0
     result = number.to_s if result ==
+    result
+  end
 end
 
 describe FizzBuzz do
@@ -22,18 +24,4 @@ describe FizzBuzz do
     expect(subject.fizz_buzz(input)).to eq(result)
   end
 end
-
-
-
-  it 'should return "2" for 2' do
-    expect(subject.fizz_buzz(2)).to eq('2')
-end
-
-  it 'should return "Fizz" for 3' do
-    expect(subject.fizz_buzz(3)).to eq("Fizz")
-  end
-
-  it 'should return "Fizz" for 6' do
-    expect(subject.fizz_buzz(6)).to eq("Fizz")
-  end
 end
